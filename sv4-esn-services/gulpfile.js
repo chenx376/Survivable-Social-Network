e49216c3c4d603b898101faff97ea3c4ed1aaf3e
liyansam <liyansam@qq.com>
2017-03-19 13:11:40 -0700

    add test infrastructure

--- sv4-esn-services/gulpfile.js
+++ sv4-esn-services/gulpfile.js
@@ -3,6 +3,7 @@
  */
 var gulp = require('gulp');
 var server = require('gulp-express');
+var mocha = require('gulp-mocha');
 
 gulp.task('serve', function () {
     // Start the server at the beginning of the task
@@ -11,3 +12,16 @@ gulp.task('serve', function () {
     //Watch for changes
     gulp.watch(['server.js', '**/*.js'], [server.run]);
 });
+
+gulp.task('test', () =>
+    gulp.src(['tests/**/*.js'], {read: false})
+    // `gulp-mocha` needs filepaths so you can't have any plugins before it
+    .pipe(
+        mocha({
+            reporter: 'spec',
+            quiet: false,
+            clearRequireCache: false,
+            ui: 'tdd'
+        })
+    )
+);
