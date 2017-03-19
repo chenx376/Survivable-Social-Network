a14c79e71c8f87980c77b66cdecdd5b4352b2aef
liyansam <liyansam@qq.com>
2017-03-19 16:34:31 -0700

    delete the NODE_ENV statement in server.js

--- sv4-esn-services/server.js
+++ sv4-esn-services/server.js
@@ -29,8 +29,6 @@ var chat = require('./chat.js')(io);
 
 var conn = new ConnectionController();
 
-var env = process.env.NODE_ENV || 'dev';
-
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
