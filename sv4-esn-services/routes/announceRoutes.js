7d60f6f3355be6c8b77c6efb119db4e86e8b8dab
Xiao Chen <chenx376@outlook.com>
2017-03-19 14:54:39 -0700

    Announce part almost finished

---
+++ sv4-esn-services/routes/announceRoutes.js
@@ -0,0 +1,34 @@
+/**
+ * Created by xiaochen on 3/19/17.
+ */
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var announceController = require ('../controllers/announceController.js');
+
+/*
+ * GET
+ */
+router.get('/', passport.authenticate('jwt', { session: false }), announceController.list);
+
+/*
+ * GET
+ */
+router.get('/:id', passport.authenticate('jwt', { session: false }),  announceController.show);
+
+/*
+ * POST
+ */
+router.post('/',  announceController.create);
+
+/*
+ * PUT
+ */
+router.put('/:id', passport.authenticate('jwt', { session: false }),  announceController.update);
+
+/*
+ * DELETE
+ */
+router.delete('/:id', passport.authenticate('jwt', { session: false }),  announceController.remove);
+
+module.exports = router;
