4642a91715e5d7f46c27bfa803d86f9c72dfa394
Xiao Chen <chenx376@outlook.com>
2017-03-19 23:03:00 -0700

    Useronline-offline bug solve

--- sv4-esn-services/dao/userDao.js
+++ sv4-esn-services/dao/userDao.js
@@ -105,7 +105,7 @@ module.exports = class UserDAO {
             user.role = userToUpdate.role ? userToUpdate.role : user.role;
             user.status = userToUpdate.status ? userToUpdate.status: user.status;
             user.status_information = userToUpdate.status_information ? userToUpdate.status_information : user.status_information;
-            user.online = userToUpdate.online? true: false;
+            user.online = userToUpdate.online? userToUpdate.online : user.online;
             user.location = userToUpdate.location? userToUpdate.location: user.location;
 
             user.save(function (err, user) {
