a5c3bf001fee06caf69a01a133888b34c2c8c68d
liyansam <liyansam@qq.com>
2017-03-20 15:46:44 -0700

    add tests for userDao

--- sv4-esn-services/tests/userDaoTest.js
+++ sv4-esn-services/tests/userDaoTest.js
@@ -77,15 +77,62 @@ suite('UserDAO Tests', function(){
         done();
     });
 
-    test('Updating a user', function(done){
+    test('Finding a user by ID', function(done){
+        var sha256 = createHash('sha256');
+        var shapassword = sha256.update('123456', 'utf8').digest('hex');
 
-        done();
+        let id = tmp_id;
+        userDao.findById(id, function(user){
+            expect(user.username).to.eql('yanli');
+            expect(user.email).to.eql('yanli@gmail.com');
+            expect(user.password).to.eql(shapassword);
+            expect(user.created_at).to.eql('1489962761679');
+            expect(user.updated_at).to.eql('1489962761679');
+            expect(user.role).to.eql('CITIZEN');
+            expect(user.location).to.eql('Shanghai');
+            done();
+        }, function(error){
+            expect(error).to.be(undefined);
+            done();
+        });
+    });
+
+    test('Updating a user', function(done){
+        var sha256 = createHash('sha256');
+        var shapassword = sha256.update('123456', 'utf8').digest('hex');
+
+        let user = {
+            id : tmp_id,
+            username : 'yanli',
+            email : 'new@email.com',
+            password : shapassword,
+            created_at : '1489962761679',
+            updated_at : '1489962761679',
+            role : 'CITIZEN',
+            status: 0,
+            status_information: 'status_info',
+            online: true,
+            location : 'Mountain View'
+        };
+
+        userDao.update(user, function(user){
+            expect(user.email).to.eql('new@email.com');
+            done();
+        }, function(error) {
+            expect(error).to.be(undefined);
+            done();
+        });
     });
 
 
     test('Removing a user', function(done){
-
-        done();
+        let id = tmp_id;
+        userDao.remove(id, function(){
+            done();
+        }, function(error){
+            expect(error).to.be(undefined);
+            done();
+        });
     });
 
     suiteTeardown('Teardown DB Connection', function(done){
