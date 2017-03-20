ed3d41bbb17023927396093eb87d69366e75dabd
qin <qinbingchen@me.com>
2017-03-20 15:28:36 -0700

    UI update

--- sv4-esn-view/src/app/components/chat/chat.component.ts
+++ sv4-esn-view/src/app/components/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { Subscription } from "rxjs";
 import { UserService } from '../../services/user/user.service';
 import { ChatService } from '../../services/chat/chat.service';
@@ -18,22 +18,30 @@ export class ChatComponent implements OnInit, OnDestroy {
 
   private socketConnection: Subscription;
 
+  @ViewChild('messageList') messageList: ElementRef;
+
   messages: [Message];
   message: string;
 
-  constructor(userService: UserService, chatService: ChatService) {
+  constructor(userService: UserService,
+              chatService: ChatService) {
     this.userService = userService;
-    this.chatService = chatService
+    this.chatService = chatService;
   }
 
   ngOnInit() {
     this.chatService.getPublicMessages()
       .subscribe(messages => {
-
         this.messages = messages;
+        setTimeout(() => {
+          this.messageList.nativeElement.scrollTop = this.messageList.nativeElement.scrollHeight;
+        }, 0);
         this.socketConnection = this.chatService.receiveMessage()
           .subscribe(message => {
             this.messages.push(message);
+            setTimeout(() => {
+              this.messageList.nativeElement.scrollTop = this.messageList.nativeElement.scrollHeight;
+            }, 0);
           });
       });
   }
--- sv4-esn-view/src/app/services/chat/chat.service.ts
+++ sv4-esn-view/src/app/services/chat/chat.service.ts
@@ -57,10 +57,9 @@ export class ChatService {
     let hour = ('0' + date.getHours()).slice(-2);
     let minute = ('0' + date.getMinutes()).slice(-2);
     let second = ('0' + date.getSeconds()).slice(-2);
-    let month = ('0' + date.getMonth()).slice(-2);
-    let day = ('0' + date.getDay()).slice(-2);
-
-    return `${hour}:${minute}:${second} ${month}/${day}/${date.getFullYear()}`;
+    let month = ('0' + (date.getMonth() + 1)).slice(-2);
+    let day = ('0' + date.getDate()).slice(-2);
+    return `${date.getFullYear()}/${month}/${day} ${hour}:${minute}:${second}`;
   }
 
 }
