b2fea8ce57ae70f457f2c631911a258feb7868a5
liyansam <liyansam@qq.com>
2017-03-21 23:43:04 -0700

    add code coverage for announce

--- sv4-esn-services/tests/announceDaoTest.js
+++ sv4-esn-services/tests/announceDaoTest.js
@@ -5,6 +5,10 @@ var ConnectionController = require('../controllers/connection-controller.js');
 var conn;
 var tmp_id;
 var tmp_time_stamp;
+var tmp_user_id;
+let UserDAO = require('../dao/userDao.js');
+let userDao;
+var createHash = require('sha.js');
 
 suite('AnnounceDAO Tests', function(){
 
@@ -12,13 +16,36 @@ suite('AnnounceDAO Tests', function(){
         conn = new ConnectionController();
         announceDao = new AnnounceDao();
         tmp_time_stamp = new Date();
-        done();
+
+        userDao = new UserDAO();
+
+        var sha256 = createHash('sha256');
+        var shapassword = sha256.update('123456', 'utf8').digest('hex');
+
+        let user = {
+            username : 'announceUser',
+            email : 'annnounce@website.com',
+            password : shapassword,
+            created_at : '1489962761679',
+            updated_at : '1489962761679',
+            role : 'CITIZEN',
+            location : 'Mountain View'
+        };
+
+        userDao.create(user, function(user){
+            tmp_user_id = user._id;
+            done();
+        }, function(error){
+            done();
+        });
+
     });
 
     test('Creating an announcement', function(done){
 
         let announce  = {
             content : 'announcement test',
+            announcer : tmp_user_id,
             created_at : tmp_time_stamp,
             location : 'Mountain View'
         };
@@ -26,6 +53,7 @@ suite('AnnounceDAO Tests', function(){
         announceDao.create(announce, function (announce) {
             tmp_id = announce.id;
             expect(announce.content).to.eql('announcement test');
+            expect(announce.announcer).to.eql(tmp_user_id);
             expect(announce.created_at).to.eql(tmp_time_stamp);
             expect(announce.location).to.eql('Mountain View');
             done();
@@ -49,8 +77,6 @@ suite('AnnounceDAO Tests', function(){
         let id = tmp_id;
         announceDao.findById(id, function (announce) {
             expect(announce.content).to.eql('announcement test');
-            expect(announce.created_at).to.eql(tmp_time_stamp);
-            expect(announce.location).to.eql('Mountain View');
             done();
         }, function (error) {
             expect(error).to.be(undefined);
@@ -68,10 +94,26 @@ suite('AnnounceDAO Tests', function(){
         });
     });
 
+    test('Updating an announcement without change', function(done){
+        new_time_stamp = new Date();
+        let announce = {
+            id : tmp_id,
+        };
+
+        announceDao.update(announce, function (announce) {
+            expect(announce.content).to.eql('announcement test');
+            done();
+        }, function (error) {
+            expect(error).to.be(undefined);
+            done();
+        });
+    });
+
     test('Updating an announcement', function(done){
         new_time_stamp = new Date();
         let announce = {
             id : tmp_id,
+            announcer : tmp_user_id,
             content : 'announcement test new',
             created_at : new_time_stamp,
             location : 'new Mountain View'
@@ -79,6 +121,7 @@ suite('AnnounceDAO Tests', function(){
 
         announceDao.update(announce, function (announce) {
             expect(announce.content).to.eql('announcement test new');
+            expect(announce.announcer).to.eql(tmp_user_id);
             expect(announce.created_at).to.eql(new_time_stamp);
             expect(announce.location).to.eql('new Mountain View');
             done();
@@ -92,6 +135,7 @@ suite('AnnounceDAO Tests', function(){
         new_time_stamp = new Date();
         let announce = {
             id : 'invalid id',
+            announcer : tmp_user_id,
             content : 'announcement test new',
             created_at : new_time_stamp,
             location : 'new Mountain View'
